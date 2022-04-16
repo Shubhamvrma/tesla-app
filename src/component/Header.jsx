aa24c6a597b6832ef6d678efad66ca3bec34704f
shubham <shubhamvrma07@gmail.com>
2022-04-16 12:56:31 +0530

    Made full functional Side menu bar

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import MenuIcon from '@material-ui/icons/Menu'
+import CloseIcon from '@material-ui/icons/Close'
 
 function Header() {
+  const [burgerStatus, setBurgerStatus] = useState(false); 
+
   return (
     <Container>
         <a>
@@ -17,8 +20,18 @@ function Header() {
         <RightMenu>
             <a href="#">Shop</a>
             <a href="#">Tesla Account</a>
-            <CustomMenu />
+            <CustomMenu onClick={() => setBurgerStatus(true)}/>
         </RightMenu>   
+        <BurgerNav show={burgerStatus}>
+            <CloseWraper>
+                <CustomClose onClick={() => setBurgerStatus(false)}/>
+            </CloseWraper>
+            <li><a href="#">Existing Inventory</a></li>
+            <li><a href="#">Used Inventory</a></li>
+            <li><a href="#">Trade-in</a></li>
+            <li><a href="#">Cyber Truck</a></li>
+            <li><a href="#">Roadster</a></li>
+        </BurgerNav>
     </Container>
   )
 }
@@ -35,6 +48,7 @@ const Container = styled.div`
     top: 0;
     left: 0;
     right: 0;
+    z-index: 1;
 `
 const Menu = styled.div`
     display: flex;
@@ -62,5 +76,38 @@ const RightMenu = styled.div`
 `
 
 const CustomMenu = styled(MenuIcon)`
+    cursor: pointer;
+`
+
+const BurgerNav = styled.div`
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    background: white;
+    width: 300px;
+    z-index: 10;
+    list-style: none;
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    text-align: start;
+    transform: ${props => props.show ? 'translateX(0)' : 'translateX(100%)' };
+    li{
+        padding: 15px 0;
+        border-bottom: 1px solid rgba(0, 0, 0, .2);
+
+        a{
+            font-weight: 600;
+        }
+    }
+`
+
+const CloseWraper = styled.div`
+    display: flex;
+    justify-content: flex-end;
+`
+
+const CustomClose = styled(CloseIcon)`
     cursor: pointer;
 `
